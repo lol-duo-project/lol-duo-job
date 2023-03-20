d7a9c20535ec2f8ea5dd4fbd54f4d1555c559f13
imygnam <imygnam@gmail.com>
2023-03-20 18:18:53 +0900

    [FIX] secret 권한 이슈

--- src/main/kotlin/com/lolduo/lolduojob/config/secret/AwsSecretProd.kt
+++ src/main/kotlin/com/lolduo/lolduojob/config/secret/AwsSecretProd.kt
@@ -1,18 +1,24 @@
 package com.lolduo.lolduojob.config.secret
 
-import com.amazonaws.services.secretsmanager.AWSSecretsManager
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest
-import com.google.gson.Gson
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
 @Profile("prod")
-class AwsSecretProd (private val awsSecretsManager: AWSSecretsManager) : AwsSecret{
+class AwsSecretProd (
+    //private val awsSecretsManager: AWSSecretsManager
+    ) : AwsSecret{
     override fun getSecret(secretName: String): Any {
-        val getSecretValueRequest = GetSecretValueRequest()
-                .withSecretId(secretName)
-        val getSecretValueResult = awsSecretsManager.getSecretValue(getSecretValueRequest)
-        return Gson().fromJson(getSecretValueResult.secretString, Any::class.java)
+//        val getSecretValueRequest = GetSecretValueRequest()
+//                .withSecretId(secretName)
+//        val getSecretValueResult = awsSecretsManager.getSecretValue(getSecretValueRequest)
+//        return Gson().fromJson(getSecretValueResult.secretString, Any::class.java)
+        return when (secretName) {
+            "prod/mongoDB" -> MongoDB_Secret(
+                "mongodb://prod-mongodb-service-lol-duo:27017",
+                "mongodb://prod-mongodb-service-lol-duo:27017"
+            )
+            else -> throw RuntimeException("Secret not found")
+        }
     }
 }
